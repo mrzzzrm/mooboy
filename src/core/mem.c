147904e97d478ac35d9fe4782751bb57f7f11e7e
Moritz Eyssen <moritz@Jingizu>
2012-11-01 17:10:54 +0100

    void

---
+++ src/core/mem.c
@@ -0,0 +1,13 @@
+#include "mem.h"
+
+ram_t ram;
+rom_t rom;
+mbc_t mbc;
+
+void mem_reset() {
+
+}
+
+void mem_load_rom(u8 *data, uint size) {
+
+}
--- src/core/mem.h
+++ src/core/mem.h
@@ -4,13 +4,21 @@
     #include "util/defines.h"
 
     typedef struct ram_s {
-
+        byte wram[0x2000];
     } ram_t;
 
     typedef struct rom_s {
         byte *banks[0x4000];
     } rom_t;
 
+    typedef struct mbc_s {
+        uint romsize;
+        uint ramsize;
+    } mbc_t;
+
+    extern ram_t ram;
+    extern rom_t rom;
+    extern mbc_t mbc;
 
     void mem_reset();
     void mem_load_rom(u8 *data, uint size);
