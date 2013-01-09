b1dc8f0f16a1da0394dcc97835bc6735d867f8ac
mrzzzrm <mrzesn@t-online.de>
2013-01-09 15:43:32 +0100

    Removed some debugging stuff, that all will go to src/debug/

--- src/core/emu.c
+++ src/core/emu.c
@@ -37,29 +37,15 @@ bool emu_load(u8 *data, size_t size) {
 
 bool emu_run() {
     debug_init();
-    printf("Starting emulation\n");
 
     for(;;) {
         debug_console();
 
-        if(dbg.verbose == DBG_VLVL_PC_ONLY) {
-            fprintf(stderr,"PC=%.4X\n", cpu.pc.w);
-        }
-
-        if(dbg.verbose >= DBG_VLVL_MIN) {
-            debug_print_cpu_state();
-            if(dbg.verbose >= DBG_VLVL_NORMAL) fprintf(stderr, "{\n");
-        }
-
         debug_before();
         emu_step();
         debug_after();
-
-        if(dbg.verbose >= DBG_VLVL_MIN) {
-            if(dbg.verbose >= DBG_VLVL_NORMAL) debug_print_diff();
-            if(dbg.verbose >= DBG_VLVL_NORMAL) fprintf(stderr, "}\n");
-        }
     }
+
     return true;
 }
 
