dc47cdadefaca5c6c7a83bcf5d6dd91a42ee4a74
mrzzzrm <mrzesn@t-online.de>
2013-09-07 10:54:34 +0200

    Missing files

---
+++ src/menu/sdl/error.h
@@ -0,0 +1,7 @@
+#ifndef MENU_ERROR_H
+#define MENU_ERROR_H
+
+void menu_error_init();
+void menu_error_close();
+
+#endif // MENU_ERROR_H
---
+++ src/util/card.h
@@ -0,0 +1,7 @@
+#ifndef UTIL_CARD_H
+#define UTIL_CARD_H
+
+void card_save();
+void card_load();
+
+#endif
